8c8781420695abb99428f3eaae31910fdfefb5a7
thanhtran1259 <thanhtran1259@gmail.com>
2025-06-18 20:09:21 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ const FirebaseProvider = ({ children }) => {
     useEffect(() => {
         const initFirebase = async () => {
             try {
+				console.log("Firebase Config:", process.env.REACT_APP_FIREBASE_CONFIG);
                 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
                 const app = initializeApp(firebaseConfig);
                 const dbInstance = getFirestore(app);
